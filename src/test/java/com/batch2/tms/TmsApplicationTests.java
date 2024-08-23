a219fbf11d8013234ba66fed4b54e6a2fbe9f743
rajesh n <rajesh.kit@gmail.com>
2024-08-23 15:45:16 +0530

    empty project

---
+++ src/test/java/com/batch2/tms/TmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch2.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
